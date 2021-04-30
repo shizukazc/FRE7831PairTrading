005977e5ba94c726eed455f065b8460da3d4048a
DevonC <devonc@DevonCs-MacBook-Pro.local>
2021-04-29 22:57:07 -0700

    Initialized project

---
+++ FRE 7831 Pair Trading/FRE 7831 Pair Trading/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FRE 7831 Pair Trading
+//
+//  Created by DevonC on 4/29/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
